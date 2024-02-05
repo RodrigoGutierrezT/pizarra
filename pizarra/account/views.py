98bf3c848b84ba53401417646818bf1d520596a9
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-02-05 14:53:20 +0900

    Add signup functionality

--- pizarra/account/views.py
+++ pizarra/account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
+
+def signup(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+
+        if name and email and password1 and password2:
+            user = User.objects.create_user(name, email, password1)
+
+            print("User Created: ", user)
+
+            return redirect('/login/')
+
+    else:
+        print('Just show the form')
+
+    return render(request, 'account/signup.html')
